a213bdb913ceca55be94d637ec85757b535019f2
murad_simec <murad@simecsystem.com>
2022-08-21 17:30:36 +0600

    first commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.scss'
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+const Home = () => {
+    return (
+        <div className='Home'>
+            Hello !!
+            <AcUnitIcon />
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Routes/Routers.jsx
@@ -0,0 +1,20 @@
+// import React from 'react';
+// import {  BrowserRouter, Routes, Route } from "react-router-dom";
+// const Routers = () => {
+//     return (
+//         <div>
+//             <BrowserRouter>
+//                 <Routes>
+//                     <Route path="/" element={<App />} />
+//                     <Route index element={<Home />} />
+//                     <Route path="teams" element={<Teams />} />
+//                     <Route path=":teamId" element={<Team />} />
+//                     <Route path="new" element={<NewTeamForm />} />
+//                     <Route index element={<LeagueStandings />} />
+//                 </Routes>
+//             </BrowserRouter>
+//         </div>
+//     );
+// };
+
+// export default Routers;
