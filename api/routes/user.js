0fb935975b6096cfc056b11a87318822e1952ba7
murad_simec <murad@simecsystem.com>
2022-08-27 18:10:00 +0600

    user state api done

--- api/routes/user.js
+++ api/routes/user.js
@@ -10,7 +10,7 @@ router.get('/all-end-users', isPermitted, async (req, res) => {
         if (user.isAdmin) {
             const users = query ? await User.find({}, { password: 0 }).sort({ _id: -1 }).limit(5) : await User.find({}, { password: 0 })
             res.status(200).json({ status: true, users, message: "All user list fetched" })
-        } 
+        }
         else {
             res.status(403).json({ status: false, message: "You are not authorize to see all users" })
         }
@@ -88,5 +88,22 @@ router.get('/findone/:id', isPermitted, async (req, res) => {
         res.status(500).json(error.message)
     }
 })
+
+//get users count accroding to month
+router.get('/user-states', async (req, res) => {
+    const today = new Date()
+    const lastYear = today.getFullYear() - 1
+    const monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    try {
+        const user = await User.aggregate([
+            { $project: { whichMonth: { $month: "$createdAt" } } },
+            { $group: { _id: "$whichMonth", total: { $sum: 1 } } }
+        ])
+        res.status(200).json({ status: true, user, message: "profile found" })
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+})
+
 
 module.exports = router
